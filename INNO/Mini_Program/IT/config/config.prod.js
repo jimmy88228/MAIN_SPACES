188cd249de99da2e43d4d860b39677db87bc9334
jimmy88228 <447841059@qq.com>
2020-10-09 10:38:23 +0800

    delete

--- INNO/Mini_Program/IT/config/config.prod.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    LOGIN_DOMIN: "https://limitapi.itezhop.com/MainApi",
-    USER_DOMIN: "https://limitapi.itezhop.com/MainApi",
-    WXMSG_DOMIN: "https://limitapi.itezhop.com/MainApi",
-    DRAW_DOMIN: "https://api.itezhop.com/MainApi",
-    WAP_DOMIN: "https://files.itezhop.com/WebApi",
-    LOG_DOMIN: "https://limitapi.itezhop.com/LogApi",
-};
