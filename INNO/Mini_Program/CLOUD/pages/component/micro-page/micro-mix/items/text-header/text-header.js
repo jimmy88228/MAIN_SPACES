10a653ebf428126b0799b2628566fab87e76558f
jimmy88228 <447841059@qq.com>
2021-03-24 18:45:47 +0800

    update

---
+++ INNO/Mini_Program/CLOUD/pages/component/micro-page/micro-mix/items/text-header/text-header.js
@@ -0,0 +1,27 @@
+// pages/component/micro-page/items/text-header/text-header.js
+const app = getApp();
+import mcBehavior from '../../../help/mc-behavior.js'
+Component(app.BTAB({
+  behaviors: [mcBehavior],
+  properties: {
+    _data:{
+      type:Object,
+      value:{},
+      observer:function(n,o){
+        if(!this.readyed)return
+        this.init(n);
+      }
+    }
+  },
+  attached(){
+    this.readyed = true;
+  },
+  data: {
+    screenWidth:app.SIH.screenWidth
+  },
+  methods: {
+    init(data){
+      // console.log('init text-header',data)
+    }
+  }
+}))
