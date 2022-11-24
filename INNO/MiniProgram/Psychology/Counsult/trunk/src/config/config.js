8cb321a074120ae261603195b4de54a6cd8aa43e
jimmy88228 <447841059@qq.com>
2022-11-24 11:52:07 +0800

    update

--- INNO/MiniProgram/Psychology/Counsult/trunk/src/config/config.js
+++ INNO/MiniProgram/Psychology/Counsult/trunk/src/config/config.js
@@ -1,9 +1,9 @@
 import brandConfig from "./brand.config.js";
 import setConfig from "./setConfig.js";
 let config = {
-	env: 'prod', // 环境:dev, prod, test （h5的环境下，只填写dev 就可以，可以根据域名来判断）
+	env: 'dev', // 环境:dev, prod, test （h5的环境下，只填写dev 就可以，可以根据域名来判断）
 	log: true,
-	brandCode: 'SURVEY_SXFW', // SURVEY_GZHG,中华人民共和国广州海关;SURVEY_SXFW,上馨心理咨询中心; SURVEY_INNO,英朗企业eap;SURVEY_GDHD,省航道;
+	brandCode: 'SURVEY_INNO', // SURVEY_GZHG,中华人民共和国广州海关;SURVEY_SXFW,上馨心理咨询中心; SURVEY_INNO,英朗企业eap;SURVEY_GDHD,省航道;
 	platformSrc: 'WXAPP',
 	clickLimitTime: 2000, // 连续点击 毫秒
 	PAGE_SIZE:20,
