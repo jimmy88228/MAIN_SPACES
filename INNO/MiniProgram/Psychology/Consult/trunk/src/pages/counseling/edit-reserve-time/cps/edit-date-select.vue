07cc07e3697103488c7072bfcfdd94d8bccf19c7
jimmy88228 <447841059@qq.com>
2022-11-28 11:57:26 +0800

    update

---
+++ INNO/MiniProgram/Psychology/Consult/trunk/src/pages/counseling/edit-reserve-time/cps/edit-date-select.vue
@@ -0,0 +1,339 @@
+<template>
+  <view>
+    <view class="time-select-tips flex-b-c">
+      <view class="">
+        <view class="font-26 bold">选择日期</view>
+        <view class="font-18 C_7f m-t-10">显示最近30天的档期</view>
+      </view>
+      <view class="flex" @click="selectAll=!selectAll">
+        <view class="select-icon">
+          <view class="select-switch" :class="{ selected: selectAll}"></view>
+        </view>
+        <view class="font-26">全选</view>
+      </view>
+    </view>
+    <view class="reserve-time-area">
+      <view class="time-select">
+        <view v-for="(item,i) in dateList" :key="i">
+          <view class="font-26 p-t-40">{{item.month}}</view>
+          <view class="flex">
+            <view class="time-select-group">
+              <view
+                :class="['time-select-item','flex-col', 'flex','flex-c-c',dateItem.isFull && 'time-full',dateItem.selected && 'time-selected']"
+                v-for="(dateItem,dateIndex) in item.dateArr" :key="dateIndex">
+                <view :class="[dateItem.setTime?'font-20 m-b-5':'font-24 m-b-10']">{{dateItem.date}}</view>
+                <view class="font-20">{{dateItem.week}}</view>
+                <view class="font-16 C_5ecf68 m-t-10" v-if="dateItem.setTime">已设时段</view>
+                <view class="selected-icon" v-if="dateItem.selected">
+                  <view class="select-switch selected"></view>
+                </view>
+              </view>
+            </view>
+          </view>
+        </view>
+      </view>
+      <view class="button-area flex-b-c font-26">
+        <view class="left-button flex-c-c">预览</view>
+        <view class="right-button flex-c-c" @click="setTime">配置时段</view>
+      </view>
+    </view>
+    <editTimeSelect ref="popup"></editTimeSelect>
+  </view>
+</template>
+
+<script>
+  import editTimeSelect from "./edit-time-select.vue"
+  const pageOption = Page.BaseComp({
+    components: {
+      editTimeSelect
+    },
+    data() {
+      return {
+        moreIcon: '/more.png',
+        selectAll: false,
+        dateList: [{
+            month: '2月',
+            dateArr: [{
+                date: '02-16',
+                week: '今日',
+                setTime: true
+              },
+              {
+                date: '02-17',
+                week: '明日'
+              },
+              {
+                date: '02-18',
+                week: '周一'
+              },
+              {
+                date: '02-19',
+                week: '周二'
+              },
+              {
+                date: '02-20',
+                week: '周三',
+                selected: true
+              },
+              {
+                date: '02-21',
+                week: '周四'
+              },
+              {
+                date: '02-22',
+                week: '周五'
+              },
+              {
+                date: '02-16',
+                week: '今日',
+                selected: true
+              },
+              {
+                date: '02-17',
+                week: '明日'
+              },
+              {
+                date: '02-18',
+                week: '周一'
+              },
+              {
+                date: '02-19',
+                week: '周二'
+              },
+              {
+                date: '02-20',
+                week: '周三'
+              },
+              {
+                date: '02-21',
+                week: '周四'
+              },
+              {
+                date: '02-22',
+                week: '周五'
+              },
+            ]
+          },
+          {
+            month: '3月',
+            dateArr: [{
+                date: '02-16',
+                week: '今日',
+                setTime: true
+              },
+              {
+                date: '02-17',
+                week: '明日'
+              },
+              {
+                date: '02-18',
+                week: '周一'
+              },
+              {
+                date: '02-19',
+                week: '周二'
+              },
+              {
+                date: '02-20',
+                week: '周三',
+                selected: true
+              },
+              {
+                date: '02-21',
+                week: '周四'
+              },
+              {
+                date: '02-22',
+                week: '周五'
+              },
+              {
+                date: '02-16',
+                week: '今日',
+                selected: true
+              },
+              {
+                date: '02-17',
+                week: '明日'
+              },
+              {
+                date: '02-18',
+                week: '周一'
+              },
+              {
+                date: '02-19',
+                week: '周二'
+              },
+              {
+                date: '02-20',
+                week: '周三'
+              },
+              {
+                date: '02-21',
+                week: '周四'
+              },
+              {
+                date: '02-22',
+                week: '周五'
+              },
+            ]
+          }
+        ],
+      }
+    },
+    methods: {
+      setTime() {
+        this.$refs.popup.showTimeEdit()
+      }
+    },
+  })
+  export default pageOption
+</script>
+
+<style lang="scss" scoped>
+  .time-select-tips {
+    margin-bottom: 20rpx;
+    padding: 0 20rpx;
+
+    .select-icon {
+      padding: 5rpx 10rpx;
+
+      .select-switch {
+        width: 25rpx;
+        height: 25rpx;
+        background: #FFFFFF;
+        border: 2rpx solid #979797;
+        border-radius: 50%;
+        position: relative;
+      }
+
+      .selected::after {
+        content: "";
+        display: block;
+        position: absolute;
+        top: 40%;
+        left: 40%;
+        width: 30%;
+        height: 80%;
+        border-radius: 4rpx;
+        border: 6rpx solid $uni-main-color;
+        border-top-color: transparent;
+        border-left-color: transparent;
+        transform: translate(-18%, -80%) rotate(40deg);
+      }
+    }
+  }
+
+  .reserve-time-area {
+    width: 100%;
+    background: #FFFFFF;
+    border-radius: 20rpx;
+
+    .time-select {
+      width: 100%;
+      padding: 0 30rpx 0 31rpx;
+      box-sizing: border-box;
+
+      .time-select-group {
+        width: 639rpx;
+        padding-bottom: 12rpx;
+        padding-top: 25rpx;
+        display: flex;
+        flex-wrap: wrap;
+        box-sizing: border-box;
+
+        .time-select-item {
+          width: 18.122%;
+          height: 122rpx;
+          border-radius: 33rpx;
+          font-size: 21rpx;
+          box-sizing: border-box;
+          border: 2rpx solid #B2B2B2;
+          margin-right: 15rpx;
+          margin-bottom: 14rpx;
+          position: relative;
+          color: #333333;
+
+          .time-full-tips {
+            position: absolute;
+            left: 0;
+            text-align: center;
+            font-size: 18rpx;
+            top: -14rpx;
+            width: 48rpx;
+            height: 27rpx;
+            background: #F7F7F7;
+            border-radius: 6rpx;
+            border: 2rpx solid #D4D4D4;
+          }
+        }
+
+        .time-full {
+          // background: rgba($color: #F7F7F7, $alpha: 0.4);
+          color: rgba($color: #222222, $alpha: 0.4);
+          border: 2rpx solid rgba($color: #333333, $alpha: 0.4);
+        }
+
+        .time-selected {
+          background: rgba($color: $uni-main-color, $alpha: 0.05);
+          border: 1px solid $uni-main-color;
+          color: $uni-main-color;
+        }
+
+        .selected-icon {
+          // padding: 5rpx 20rpx;
+          position: absolute;
+          right: 1rpx;
+          bottom: 1rpx;
+
+          .select-switch {
+            width: 23rpx;
+            height: 23rpx;
+            background: $uni-main-color;
+            border-radius: 50%;
+            position: relative;
+          }
+
+          .selected::after {
+            content: "";
+            display: block;
+            position: absolute;
+            top: 60%;
+            left: 40%;
+            width: 20%;
+            height: 50%;
+            border-radius: 4rpx;
+            border: 2rpx solid #ffffff;
+            border-top-color: transparent;
+            border-left-color: transparent;
+            transform: translate(-18%, -80%) rotate(40deg);
+          }
+        }
+
+        .time-select-item:nth-child(5n) {
+          margin-right: 0;
+        }
+      }
+    }
+
+    .button-area {
+      padding: 0 30rpx 27rpx 31rpx;
+
+      &>view {
+        width: 311rpx;
+        height: 88rpx;
+        border: 2rpx solid #D5D5D5;
+        border-radius: 10rpx;
+      }
+
+      .left-button {
+        background: #FFFFFF;
+        color: #5f5f5f;
+      }
+
+      .right-button {
+        background: $uni-main-color;
+        color: #FFFFFF;
+      }
+    }
+  }
+</style>
---
+++ INNO/MiniProgram/Psychology/Consult/trunk/src/pages/counseling/reserve-time/reserve-time.vue
@@ -0,0 +1,93 @@
+<template>
+  <view :class="!isEmpty ? 'bg_f7' : 'bg_fff'">
+    <view class="reserve-time">
+      <page-nav :isFull="false">
+        <template slot="title"> 坐班表 </template>
+      </page-nav>
+      <view v-if="!isEmpty">
+        <view class="reserve-time-area">
+          <dateSelect></dateSelect>
+          <timeSelect></timeSelect>
+        </view>
+        <view class="bottom-area-true-height"></view>
+        <view class="bottom-area flex-c-c">
+          <view class="confirm flex-c-c">
+            确认
+          </view>
+        </view>
+      </view>
+      <view class="absolute empty" v-else>
+        <image :src="staticAddress+emptyIcon" class="empty-icon" mode="scaleToFill" />
+        <view class="C_B2 font-32">暂无可以咨询的时间噢~</view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+  import dateSelect from "./cps/date-select.vue"
+  import timeSelect from "./cps/time-select.vue"
+
+  const app = getApp();
+  const pageOption = Page.BasePage({
+    components: {
+      dateSelect,
+      timeSelect
+    },
+    data() {
+      return {
+        moreIcon: '/more.png',
+        isEmpty: false,
+        selectedTime: {
+
+        }
+      };
+    },
+    onReady() {
+
+    },
+    methods: {
+
+    },
+    onShow() {},
+  });
+  export default pageOption;
+</script>
+
+<style lang="scss" scoped>
+  .reserve-time {
+    min-height: 100vh;
+    box-sizing: border-box;
+    padding: 30rpx 24rpx;
+  }
+
+  .reserve-time-area {
+    width: 100%;
+    background: #FFFFFF;
+    border-radius: 20rpx;
+  }
+
+
+  .bottom-area-true-height {
+    height: 150rpx;
+  }
+
+  .bottom-area {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 130rpx;
+    background: #FFFFFF;
+    box-shadow: 0rpx -16rpx 20rpx 0rpx rgba($color: #000000, $alpha: 0.02);
+
+    .confirm {
+      width: 680rpx;
+      height: 100rpx;
+      border-radius: 10rpx;
+      background: $uni-main-color;
+      font-size: 28rpx;
+      color: #FFFFFF;
+    }
+  }
+</style>
